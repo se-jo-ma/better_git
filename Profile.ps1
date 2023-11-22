5b54079a7c7f7e428eb5404e70b4de2073505bcf
se-jo-ma <46336194+se-jo-ma@users.noreply.github.com>
2023-11-22 03:01:59 -0500

    Final update for now.

--- Profile.ps1
+++ Profile.ps1
@@ -1,3 +1,3 @@
 function better_git_clone {sh C:\BetterGit\commands\better_clone.sh $args}
 function bgc {sh C:\BetterGit\commands\better_clone.sh $args}
-function bgupdate {sh C:\BetterGit\commands\update.sh}
+function bgupdate {C:\BetterGit\commands\update.ps1}
---
+++ commands/update.ps1
@@ -0,0 +1,2 @@
+bgc se-jo-ma/better_git
+.\better_git\install.ps1
