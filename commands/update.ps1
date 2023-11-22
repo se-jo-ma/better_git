fd4e147eb3aba588f40daa66ca36015d80f01a98
se-jo-ma <46336194+se-jo-ma@users.noreply.github.com>
2023-11-22 03:27:21 -0500

    Fixed Updates and Removed Top BC

--- commands/update.ps1
+++ commands/update.ps1
@@ -1,3 +1,2 @@
-bgc se-jo-ma/better_git $PSScriptRoot/tmp
-$PSScriptRoot\tmp\better_git\install.ps1
-Remove-Item $PSScriptRoot\tmp
+& bgc -g se-jo-ma/better_git $PSScriptRoot\tmp
+& $PSScriptRoot\tmp\install.ps1
